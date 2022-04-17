a35c6036cf89cea608d907e153531cf34b086280
0d511v32f0x0d <brominpaul@gmail.com>
2022-04-17 04:28:38 +0000

    develop html structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="" value="" />
+        <button aria-label="todo action">done/notdone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="save todo">Save</button>
+    </form>
+
+    <form action=""method="">
+        <button>Delete</button>
+    </form>
+</div>
